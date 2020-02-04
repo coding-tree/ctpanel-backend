9cda2f3303b524b80c91a34b822929756b816412
kazbag <45149138+kazbag@users.noreply.github.com>
2020-02-04 08:44:22 +0100

    updated user model regarding to layout (statistics etc.)

--- models/user-model.js
+++ models/user-model.js
@@ -14,6 +14,13 @@ const userSchema = new Schema({
   userFacebook: String,
   userGithub: String,
   userDiscord: String,
+  userPresence: String,
+  userTimeSpent: String,
+  userAverageMark: String,
+  userMaxMark: Number,
+  userMinMark: Number,
+  userLongestTime: String,
+  userLeadershipAmount: Number,
   // to hash
   userPassword: String,
 });
--- readme.md
+++ readme.md
@@ -84,7 +84,14 @@ User
 - userFacebook: String,
 - userGithub: String,
 - userDiscord: String,
-- userPassword: String,
+- userPresence: String,
+- userTimeSpent: String,
+- userAverageMark: String,
+- userMaxMark: Number,
+- userMinMark: Number,
+- userLongestTime: String,
+- userLeadershipAmount: Number,
+- userPassword: String
 
 Meeting
 
