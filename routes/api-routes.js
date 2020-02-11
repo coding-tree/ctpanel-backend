f97f8dbb8718308e78ea25fac60841cac41d7e52
kazbag <45149138+kazbag@users.noreply.github.com>
2020-02-11 11:10:26 +0100

    separated pagination function into new folder aka 'middleware'

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 // API
-
 const MeetingModel = require('../models/meeting-model');
 const TopicModel = require('../models/topic-model');
+// middleware
+const paginatedResults = require('../middleware/paginate')
 // #region meetings
 
 router.get('/', (req, res) => {
@@ -196,39 +197,5 @@ router.put('/topics/vote/:id', async (req, res) => {
     }
 });
 // #endregion
-
-// pagination
-function paginatedResults(model) {
-    return async (req, res, next) => {
-        const page = parseInt(req.query.page)
-        const limit = parseInt(req.query.limit)
-
-        const startIndex = (page - 1) * limit
-        const endIndex = page * limit
-
-        const results = {}
-
-        if (endIndex < await model.countDocuments().exec()) {
-            results.next = {
-                page: page + 1,
-                limit: limit
-            }
-        }
-
-        if (startIndex > 0) {
-            results.previous = {
-                page: page - 1,
-                limit: limit
-            }
-        }
-        try {
-            results.results = await model.find().limit(limit).skip(startIndex).exec()
-            res.paginatedResults = results
-            next()
-        } catch (e) {
-            res.status(500).json({ message: e.message })
-        }
-    }
-}
 
 module.exports = router;
