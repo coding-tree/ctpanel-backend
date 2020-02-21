0f8442247359ea61ead59b741c6dcc93103235ee
freestyle09 <jozefrzadkosz09@gmail.com>
2020-02-21 11:15:01 +0100

    removed view engine and routes

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const passwordSetup = require('./config/passport-setup');
-const mongoOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+const mongoOptions = {useNewUrlParser: true, useUnifiedTopology: true};
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const configuration = require('./config/configuration');
@@ -10,18 +9,12 @@ const cookieParser = require('cookie-parser');
 
 //routes
 const authRoutes = require('./routes/auth-routes');
-const profileRoutes = require('./routes/profile-routes');
-const scheduleRoutes = require('./routes/schedule-routes');
-const topicDatabaseRoutes = require('./routes/topic-database-routes');
-const meetingsArchiveRoutes = require('./routes/meetings-archive-routes');
 const apiRoutes = require('./routes/api-routes');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
-// set view engine
-app.set('view engine', 'ejs');
 app.use(
   cookieSession({
     maxAge: 24 * 60 * 60 * 1000,
@@ -50,12 +43,4 @@ mongoose
 
 // set up routes
 app.use('/auth', authRoutes);
-app.use('/profile', profileRoutes);
-app.use('/schedule', scheduleRoutes);
-app.use('/topic-database', topicDatabaseRoutes);
-app.use('/meetings-archive', meetingsArchiveRoutes);
-app.use(apiRoutes)
-// create home route
-app.get('/', (req, res) => {
-  res.render('home', { user: req.user });
-});
+app.use(apiRoutes);
