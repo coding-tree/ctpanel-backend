9fa22cbeb70f60c8c91d868bbd28155c89f7854f
kazbag <kazimierz.bagrowski@microsoft.wsei.edu.pl>
2020-04-02 13:02:55 +0200

    Fix auth

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const express = require('express');
-const mongoOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+const mongoOptions = {useNewUrlParser: true, useUnifiedTopology: true};
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const configuration = require('./config/configuration');
 const passport = require('passport');
 const Auth0Strategy = require('passport-auth0');
+const {requiresAuth} = require('express-openid-connect');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
@@ -18,12 +19,12 @@ const querystring = require('querystring');
 //routes
 const authRoutes = require('./routes/auth-routes');
 const apiRoutes = require('./routes/api-routes');
-const topicsRoutes = require('./routes/topics-routes')
-const meetingsRoutes = require('./routes/meetings-routes')
+const topicsRoutes = require('./routes/topics-routes');
+const meetingsRoutes = require('./routes/meetings-routes');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
 app.use(cookieParser());
@@ -40,22 +41,19 @@ const auth0Config = {
   clientID: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
   callbackURL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3001/callback',
-}
+};
 
-console.log("Auth0 Configuration", auth0Config);
+console.log('Auth0 Configuration', auth0Config);
 
 // Configure Passport to use Auth0
-const strategy = new Auth0Strategy(
-  auth0Config,
-  function (accessToken, refreshToken, extraParams, profile, done) {
-    return done(null, profile, extraParams.id_token);
-  }
-);
-passport.serializeUser(function (user, done) {
+const strategy = new Auth0Strategy(auth0Config, function(accessToken, refreshToken, extraParams, profile, done) {
+  return done(null, profile, extraParams.id_token);
+});
+passport.serializeUser(function(user, done) {
   done(null, user);
 });
 
-passport.deserializeUser(function (user, done) {
+passport.deserializeUser(function(user, done) {
   done(null, user);
 });
 passport.use(strategy);
@@ -88,18 +86,25 @@ app.use(meetingsRoutes);
 function secured(req, res, next) {
   const token = req.cookies['auth0-token'];
   if (!token && !req.user) {
-    return res.status(401).send('Access Denied. No token provided')
+    res.status(401).send('Access Denied. No token provided');
   }
   next();
-  req.session.returnTo = req.originalUrl;
 }
 
-app.get('/user', secured, function (req, res) {
+app.get('/check', secured, (req, res) => {
+  console.log(req);
   if (req.user) {
-    const { displayName, id, nickname, picture } = req.user
-    res.json({ displayName, id, nickname, picture, role: 'user' });
+    return res.send('ok');
   }
-  res.redirect('http://localhost:3000/');
+  return res.redirect('http://localhost:3001/login');
+});
+
+app.get('/user', secured, function(req, res) {
+  if (req.user) {
+    const {displayName, id, nickname, picture} = req.user;
+    return res.json({displayName, id, nickname, picture, role: 'user'});
+  }
+  return res.redirect('http://localhost:3001/login');
 });
 
 // Perform the login, after login Auth0 will redirect to callback
@@ -108,13 +113,13 @@ app.get(
   passport.authenticate('auth0', {
     scope: 'openid email profile',
   }),
-  function (req, res) {
+  function(req, res) {
     res.redirect('/');
   }
 );
 
-app.get('/callback', function (req, res, next) {
-  passport.authenticate('auth0', function (err, user, info) {
+app.get('/callback', function(req, res, next) {
+  passport.authenticate('auth0', function(err, user, info) {
     if (err) {
       return next(err);
     }
@@ -122,11 +127,11 @@ app.get('/callback', function (req, res, next) {
       return res.redirect('/login');
     }
 
-    req.logIn(user, function (err) {
+    req.logIn(user, function(err) {
       if (err) {
         return next(err);
       }
-      res.cookie('auth0-token', info, { httpOnly: true });
+      res.cookie('auth0-token', info, {httpOnly: true});
       const returnTo = req.session.returnTo;
       delete req.session.returnTo;
 
@@ -138,11 +143,14 @@ app.get('/callback', function (req, res, next) {
 // app.get('/', function (req, res, next) {
 //   res.redirect('http://localhost:3000');
 // });
-
+// profile
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.openid.user));
+});
 // * LOGOUT
 app.get('/logout', (req, res) => {
-  res.clearCookie('auth0-token', { path: '/' });
-  res.clearCookie('auth0.is.authenticated', { path: '/' });
+  res.clearCookie('auth0-token', {path: '/'});
+  res.clearCookie('auth0.is.authenticated', {path: '/'});
   req.logout();
 
   var returnTo = req.protocol + '://' + req.hostname;
@@ -159,4 +167,3 @@ app.get('/logout', (req, res) => {
 
   res.redirect('http://localhost:3000/login');
 });
-
